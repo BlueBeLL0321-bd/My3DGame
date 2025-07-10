6644ab8bf2fb72252ea5fe96887ba281071a4a9e
BlueBeLL0321-bd <leeyongwoo736@gmail.com>
2025-07-10 15:44:34 +0900

    이펙트 툴 기초 및 작업

---
+++ Assets/My3DGame/Scripts/GameData/EffectList.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MyTool.GameData
+{
+	public enum EffectList
+	{
+		None = -1,
+		
+    EffectA = 0,
+    EffectB = 1,
+    NewData2 = 2,
+    NewData = 3,
+
+	}
+}
